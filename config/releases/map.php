ede8064fab5a95fa6417fb35299f8721789a7dac
dogmatic69 <dogmatic69@gmail.com>
2010-11-11 17:18:29 +0000

    creating a release for the tiny mce editor plugin

---
+++ config/releases/map.php
@@ -0,0 +1,6 @@
+<?php
+$map = array(
+	1 => array(
+		'000001_wysiwyg_tiny_mce' => 'R4cdc235e061c478199b0247a6318cd70'),
+);
+?>
---
+++ config/schema/schema.php
@@ -0,0 +1,15 @@
+<?php 
+/* SVN FILE: $Id$ */
+/* WysiwygTinyMce schema generated on: 2010-11-11 17:11:50 : 1289495390*/
+class WysiwygTinyMceSchema extends CakeSchema {
+	var $name = 'WysiwygTinyMce';
+
+	function before($event = array()) {
+		return true;
+	}
+
+	function after($event = array()) {
+	}
+
+}
+?>
