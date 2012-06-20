dd29a202cdc3dbb5ada1dba7b18aa8438c68f799
dogmatic69 <dogmatic69@gmail.com>
2012-06-21 00:55:56 +0100

    fixing formatting

--- Lib/WysiwygTinyMceEvents.php
+++ Lib/WysiwygTinyMceEvents.php
@@ -3,16 +3,16 @@
 	 * events for ck_editor
 	 */
 	class WysiwygTinyMceEvents{
-		function onRegisterWysiwyg($event){
+		function onRegisterWysiwyg($event) {
 			return 'tiny_mce';
 		}
 
-		function onRequireJavascriptToLoad($event, $data){
-			if(Configure::read('Wysiwyg.editor') != 'tiny_mce'){
+		function onRequireJavascriptToLoad($event, $data) {
+			if(Configure::read('Wysiwyg.editor') != 'tiny_mce') {
 				return false;
 			}
 
-			switch(isset($data['admin']) && $data['admin']){
+			switch(isset($data['admin']) && $data['admin']) {
 				case $data['action'] == 'admin_edit':
 				case $data['action'] == 'admin_add':
 					return 'WysiwygTinyMce.tiny_mce';
