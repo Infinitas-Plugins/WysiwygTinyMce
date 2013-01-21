dcb62823bd319a77a73acac34b6047007de8b7b5
dogmatic69 <dogmatic69@gmail.com>
2013-01-21 06:56:27 +0000

    adding coverage filter

--- Test/Case/AllWysiwygTinyMceTestsTest.php
+++ Test/Case/AllWysiwygTinyMceTestsTest.php
@@ -1,5 +1,7 @@
 <?php
-class AllWysiwygTinyMceViewCounterTestsTest extends PHPUnit_Framework_TestSuite {
+App::uses('AllTestsBase', 'Test/Lib');
+
+class AllWysiwygTinyMceViewCounterTestsTest extends AllTestsBase {
 
 /**
  * Suite define the tests for this suite
